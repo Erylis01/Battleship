e121ca4acbc3b91536b01265bb7a57de9bc1988b
Pierre <pierre.coquerel@mines-ales.org>
2016-11-29 21:30:35 +0100

    ajout de la console et pseudo

--- src/view/Draughtboard.java
+++ src/view/Draughtboard.java
@@ -9,6 +9,7 @@ import sun.rmi.runtime.Log;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.WindowConstants;
 
 import client.clientController;
@@ -19,6 +20,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 import model.Board;
+import javax.swing.JTextPane;
 
 
 public class Draughtboard extends JFrame {
@@ -29,6 +31,9 @@ public class Draughtboard extends JFrame {
 	private JTextField textFieldIp;
 	private JTextField textFieldPort;
 	private JTextField textField;
+	private JTextField textField_pseudo;
+	private JTextArea console;
+	private Board boardLeft;
 
 	public Draughtboard() {
 
@@ -61,7 +66,7 @@ public class Draughtboard extends JFrame {
 
 		// Add a TextField to set the ip address
 		textFieldIp = new JTextField("127.0.0.1");
-		textFieldIp.setBounds(92, 96, 146, 27);
+		textFieldIp.setBounds(92, 75, 146, 27);
 		panel.add(textFieldIp);
 		textFieldIp.setColumns(10);
 
@@ -80,8 +85,18 @@ public class Draughtboard extends JFrame {
 		// Add a Label for the port TextField
 		JLabel lblPort = new JLabel("Port :");
 		lblPort.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblPort.setBounds(35, 97, 40, 21);
+		lblPort.setBounds(32, 76, 40, 21);
 		panel.add(lblPort);
+		
+		textField_pseudo = new JTextField("");
+		textField_pseudo.setColumns(10);
+		textField_pseudo.setBounds(92, 113, 146, 27);
+		panel.add(textField_pseudo);
+		
+		JLabel lblPseudo = new JLabel("Pseudo :");
+		lblPseudo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPseudo.setBounds(12, 120, 63, 20);
+		panel.add(lblPseudo);
 
 		// Add a Button for the connection
 		JButton btnNewButton = new JButton("Connexion");
@@ -89,10 +104,10 @@ public class Draughtboard extends JFrame {
 		panel.add(btnNewButton);
 
 		// Drawing of the left grid
-		Board boardLeft = new Board(10, 10, 48, 195, 340, 340);
-		boardLeft.addBoxes();
-		panel.add(boardLeft);
-		clientController.setBoard(boardLeft);
+		this.boardLeft = new Board(10, 10, 48, 195, 340, 340);
+		this.boardLeft.addBoxes();
+		panel.add(this.boardLeft);
+		clientController.setBoard(this.boardLeft);
 
 		// Drawing of the right grid
 		Board boardRight = new Board(10, 10, 550, 195, 340, 340);
@@ -100,15 +115,68 @@ public class Draughtboard extends JFrame {
 		panel.add(boardRight);
 		clientController.setBoardOpponent(boardRight);
 		
+		this.console = new JTextArea();
+		this.console.setBounds(550, 35, 371, 87);
+		this.console.setEditable(false);
+		panel.add(this.console);
+		
 		// Adding a Background
 		JLabel lblFond = new JLabel("fond");
 		lblFond.setIcon(new ImageIcon(Log.class.getResource("/assets/fond_logpan.jpg")));
-		lblFond.setBounds(0, 0, 994, 571);
+		lblFond.setBounds(-10, 0, 994, 571);
 		panel.add(lblFond);
 
 		// Packing and displaying the JFrame
 		this.pack();
 		this.setVisible(true);
 
+	}
+
+	public JTextField getTextFieldIp() {
+		return textFieldIp;
+	}
+
+	public void setTextFieldIp(JTextField textFieldIp) {
+		this.textFieldIp = textFieldIp;
+	}
+
+	public JTextField getTextFieldPort() {
+		return textFieldPort;
+	}
+
+	public void setTextFieldPort(JTextField textFieldPort) {
+		this.textFieldPort = textFieldPort;
+	}
+
+	public JTextField getTextField() {
+		return textField;
+	}
+
+	public void setTextField(JTextField textField) {
+		this.textField = textField;
+	}
+
+	public JTextField getTextField_pseudo() {
+		return textField_pseudo;
+	}
+
+	public void setTextField_pseudo(JTextField textField_pseudo) {
+		this.textField_pseudo = textField_pseudo;
+	}
+
+	public JTextArea getConsole() {
+		return console;
+	}
+
+	public void setConsole(JTextArea console) {
+		this.console = console;
+	}
+
+	public Board getBoardleft() {
+		return boardLeft;
+	}
+
+	public void setBoardleft(Board boardleft) {
+		this.boardLeft = boardleft;
 	}
 }
